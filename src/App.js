19567c4e2f1c38ddeb96ef512d6454cc5577d397
varrbor <varrbor@ukr.net>
2023-12-28 19:53:30 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          test33333 <code>src/App.js</code> and save to reload.
+          test777777 <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
